f9a4ff3f514a19888120abe299c90cfc15091410
vc12345 <38955941+vc12345@users.noreply.github.com>
2026-03-19 02:20:08 +0000

    Create supabase-config.js

---
+++ js/supabase-config.js
@@ -0,0 +1,4 @@
+// js/supabase-config.js
+const _supabaseUrl = 'https://cyfjoevfxfqkgokriarz.supabase.co';
+const _supabaseAnonKey = 'sb_publishable_HNUqKbQgJ3j-OMrcHS27CQ_A_q4QBoA';
+const supabase = supabase.createClient(_supabaseUrl, _supabaseAnonKey);

ab2929d734bb0162b50b9dc19084d58216f58a75
vc12345 <38955941+vc12345@users.noreply.github.com>
2026-03-19 12:52:32 +0000

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,4 +1,4 @@
 // js/supabase-config.js
 const _supabaseUrl = 'https://cyfjoevfxfqkgokriarz.supabase.co';
-const _supabaseAnonKey = 'sb_publishable_HNUqKbQgJ3j-OMrcHS27CQ_A_q4QBoA';
+const _supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN5ZmpvZXZmeGZxa2dva3JpYXJ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4Nzk5NjYsImV4cCI6MjA4OTQ1NTk2Nn0.PLGiu7waa2ac1xw0AhGWEFrwDL8ySKrHj29gvevXH2Q';
 const supabaseClient = supabase.createClient(_supabaseUrl, _supabaseAnonKey);
